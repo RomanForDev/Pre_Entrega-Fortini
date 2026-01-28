3c4ed2246bdc9a3a70f2333cc9b2f5ef4ec348fb
RomanForDev <romanfortinidev@gmail.com>
2026-01-28 10:20:20 -0300

    Activo port 8080. Levantado el servidor con NODEMON.

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // GO GO GO
 
+import express from 'express'
+
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log("Puerto activo escuchando en 8080!.");
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>Bienvenido!</h1>")
+})
