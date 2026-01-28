6048ac3c585cc3bda3072b19dcaaa9dbe38ec833
RomanForDev <romanfortinidev@gmail.com>
2026-01-28 11:08:43 -0300

    Endpoint /products con fetch a json local funcionando y mostrando los datos.

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // GO GO GO
 
-import express from 'express'
+import express from 'express';
+import fs from 'fs';
+
 
 const app = express();
 const PORT = 8080;
 
+//FETCH!
+const rawdata = fs.readFileSync('./data/data.json');
+const data = JSON.parse(rawdata);
+const stock = JSON.stringify(data)
+console.log(stock);
+
+//Puerto activo
 app.listen(PORT, () => {
     console.log("Puerto activo escuchando en 8080!.");
 })
 
+
+//Endpoints
 app.get('/', (req, res) => {
     res.send("<h1>Bienvenido!</h1>")
+})
+
+app.get('/products', (req, res) => {
+    res.json(`${stock}`)
 })
